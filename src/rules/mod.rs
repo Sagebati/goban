7bd0e8189f94bff3beb07c8b3a197552215bd6ae
Samuel Batissou <samuelbatissou@gmail.com>
2019-02-08 14:15:09 +0100

    Changed the api for more nicesseness

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -9,14 +9,32 @@ pub mod turn {
     pub const BLACK: bool = false;
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Player {
+    White,
+    Black,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum EndGame {
+    Score(f32, f32),
+    WinnerByResign(Player),
+}
+
+#[derive(Clone, Eq, PartialEq, Debug)]
 pub enum PlayError {
     Ko,
     Suicide,
     GamePaused,
 }
 
-pub trait Rule {
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub enum Rule {
+    JapRule,
+    ChineseRule,
+}
+
+pub trait RuleImpl {
     ///
     /// Counts the point for each player.
     /// (black points, white points)
@@ -28,51 +46,46 @@ pub trait Rule {
     fn move_validation(game: &Game, stone: &Stone) -> Option<PlayError>;
 }
 
+impl Rule {
+    pub fn count_points(&self, game: &Game) -> (f32, f32) {
+        match self {
+            Rule::JapRule => {
+                let mut scores = game.calculate_territories();
+                scores.0 += game.prisoners().0 as f32;
+                scores.1 += game.prisoners().1 as f32;
+                scores.1 += game.komi();
 
-///
-/// Struct to identify the japanese rules.
-///
-pub struct JapRule;
-
-impl Rule for JapRule {
-    fn count_points(game: &Game) -> (f32, f32) {
-        let mut scores = game.calculate_territories();
-        scores.0 += game.prisoners().0 as f32;
-        scores.1 += game.prisoners().1 as f32;
-        scores.1 += game.komi();
-
-        scores
-    }
-
-    fn move_validation(game: &Game, stone: &Stone) -> Option<PlayError> {
-        if game.is_suicide(stone) {
-            return Some(PlayError::Suicide);
-        } else if game.is_ko(stone) {
-            return Some(PlayError::Ko);
-        } else {
-            None
+                scores
+            }
+            Rule::ChineseRule => {
+                // Territories in seki are not counted
+                let mut scores = game.calculate_territories();
+                scores.1 += game.komi();
+                scores
+            }
         }
     }
-}
 
-
-pub struct ChineseRule;
-
-impl Rule for ChineseRule {
-    fn count_points(game: &Game) -> (f32, f32) {
-        // Territories in seki are not counted
-        let mut scores = game.calculate_territories();
-        scores.1 += game.komi();
-        scores
-    }
-
-    fn move_validation(game: &Game, stone: &Stone) -> Option<PlayError> {
-        if game.is_suicide(stone) {
-            Some(PlayError::Suicide)
-        } else if game.is_ko(stone) {
-            Some(PlayError::Ko)
-        } else {
-            None
+    pub fn move_validation(&self, game: &Game, stone: &Stone) -> Option<PlayError> {
+        match self {
+            Rule::JapRule => {
+                if game.is_suicide(stone) {
+                    Some(PlayError::Suicide)
+                } else if game.is_ko(stone) {
+                    Some(PlayError::Ko)
+                } else {
+                    None
+                }
+            }
+            Rule::ChineseRule => {
+                if game.is_suicide(stone) {
+                    return Some(PlayError::Suicide);
+                } else if game.is_ko(stone) {
+                    return Some(PlayError::Ko);
+                } else {
+                    None
+                }
+            }
         }
     }
 }
