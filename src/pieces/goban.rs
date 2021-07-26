68338c5d9c4e15d0f8fed1237ed0582d622e7e6d
Samuel Batissou <samuelbatissou@gmail.com>
2021-07-26 19:40:09 +0200

    slef doc code

--- src/pieces/goban.rs
+++ src/pieces/goban.rs
@@ -353,7 +353,7 @@ impl Goban {
     /// adjacent string of not the same color.
     pub fn remove_go_string(&mut self, ren_to_remove_idx: GoStringIndex) {
         let color_of_the_string = self.go_strings[ren_to_remove_idx].color;
-        let mut updates: AHashMap<usize, Vec<usize>> = AHashMap::new();
+        let mut updates: AHashMap<GoStringIndex, Vec<usize>> = AHashMap::new();
 
         for point_idx in iter_stones!(self, ren_to_remove_idx) {
             for neighbor_str_idx in self
